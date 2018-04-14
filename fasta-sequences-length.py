8e7a87e2b7eabf13418873ab0f22bcfb243c8973
NielQC <dacaba.92@gmail.com>
2018-04-15 00:57:24 +0200

    harry_plotter_2

--- fasta-sequences-length.py
+++ fasta-sequences-length.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 import sys
 
 delim = ">"
-
+sub = True
+min_len_value = 5000
 
 def fasta2list (path, delim):
 	a = open(path, "r")
@@ -43,9 +44,32 @@ def harry_plotter (x, y):
 	plt.bar(x, y)
 	plt.show()
 
+def harry_plotter_2 (x, y, threshold):
+
+	lower_x = list()
+	lower_y = list()
+	higher_x = list()
+	higher_y = list()
+
+	for i in range(len(x)):
+		if x < threshold:
+			lower_x.append(x[i])
+			lower_y.append(y[i])
+		else:
+			higher_x.append(x[i])
+			higher_y.append(y[i])
+
+	f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+	ax1.bar(lower_x, lower_y)
+	ax1.set_title('Sharing Y axis')
+	ax2.bar(higher_x, higher_y)
+
 
 
 input = sys.argv[1]
 lista = fasta2list(input, delim)
 longitudes, conteos = vector_longitudes(lista)
-harry_plotter(longitudes, conteos)
+harry_plotter_2(longitudes, conteos, min_len_value)
+
+
+
