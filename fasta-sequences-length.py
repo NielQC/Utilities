7552f87c1bcb6ba79ea12cd404ca50ecde583b5b
NielQC <dacaba.92@gmail.com>
2018-04-15 11:08:13 +0200

    Nueva version. Ahora reporta en el terminal con colores

--- fasta-sequences-length.py
+++ fasta-sequences-length.py
@@ -4,8 +4,8 @@
 
 # Script para reportar la longitud de las secuencias en un fasta.
 
-import matplotlib.pyplot as plt
 import sys
+from termcolor import *
 
 delim = ">"
 sub = True
@@ -16,60 +16,31 @@ def fasta2list (path, delim):
 	b = a.read().split(delim)
 	return b
 
-def vector_longitudes (lista_archivo):
-	dic = dict()
+def reportar_longitudes (lista_archivo):
+	
+	higher_100k = 0
+	lower_500 = 0
 
+	total = 0	
 	for seq in lista_archivo:
+		total += 1
 		lineas = seq.split("\n")
-		longi = len("".join(lineas[1:]))
+		if len("".join(lineas[1:])) != 0:
+		
+			if len("".join(lineas[1:])) > 100000:
+				higher_100k += 1
+		
+			if len("".join(lineas[1:])) < 500:
+				lower_500 += 1
 
-		if longi in dic:
-			dic[longi] += 1
-		else:
-			dic[longi] = 1
-
-	x = list()
-	y = list()
-
-	for k, v in dic.items():
-		x.append(k)
-		y.append(v)
-
-	return x, y
-
-def harry_plotter (x, y):
-
-	print x
-	print y
-	plt.bar(x, y)
-	plt.show()
-
-def harry_plotter_2 (x, y, threshold):
-
-	lower_x = list()
-	lower_y = list()
-	higher_x = list()
-	higher_y = list()
-
-	for i in range(len(x)):
-		if x < threshold:
-			lower_x.append(x[i])
-			lower_y.append(y[i])
-		else:
-			higher_x.append(x[i])
-			higher_y.append(y[i])
-
-	f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-	ax1.bar(lower_x, lower_y)
-	ax1.set_title('Sharing Y axis')
-	ax2.bar(higher_x, higher_y)
+			print len("".join(lineas[1:]))
 
 
-
-input = sys.argv[1]
-lista = fasta2list(input, delim)
-longitudes, conteos = vector_longitudes(lista)
-harry_plotter_2(longitudes, conteos, min_len_value)
-
+	print
+	print colored("Un total de ", "yellow", attrs=["bold"]) + colored(str(total), "yellow", attrs=["bold", "underline"]) + colored(" contigs:", "yellow", attrs=["bold"])
+	print colored("\t-", "yellow", attrs=["bold"]) + colored(str(higher_100k), "yellow", attrs=["bold"]) + colored(" superiores a 100k pb.", "yellow", attrs=["bold"])
+	print colored("\t-", "yellow", attrs=["bold"]) + colored(str(lower_500), "yellow", attrs=["bold"]) + colored(" inferiores a 500 pb.", "yellow", attrs=["bold"])
 
 
+a = fasta2list(sys.argv[1], delim)
+reportar_longitudes(a)
